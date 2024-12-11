9bea16ce474634f6829ea615d13a0f017ec5938e
Patrick Boursier <patrick.boursier@orange.com>
2024-12-11 13:53:56 +0100

    tp module 1

--- scope_lab.js
+++ scope_lab.js
@@ -14,3 +14,22 @@ const blockConst = "Je suis une const avec portée de bloc";
 console.log(globalVar); // Sortie : "Je suis une variable globale"
 console.log(globalLet); // Sortie : "Je suis aussi global, mais avec let"
 console.log(globalConst); // Sortie : "Je suis une constante globale"
+
+//Block Scope
+console.log(blockVar);
+console.log(blockLet);
+
+function show(){
+    var functionVar = "Je suis une var à portée de bloc";
+    let functionLet = "Je suis un let à portée de bloc";
+    const functionConst = "Je suis un const à portée de bloc";
+    }
+
+    show();
+    
+console.log(functionVar); // Lance une ReferenceError
+    
+console.log(functionLet); // Lance une ReferenceError
+console.log(functionConst); // Lance une ReferenceError
+
+
