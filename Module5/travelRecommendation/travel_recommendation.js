8f595f715b77ee409f74706497bf7c0a1a5aaffe
Patrick Boursier <patrick.boursier@orange.com>
2025-01-02 17:36:00 +0100

    about us screen

--- Module5/travelRecommendation/travel_recommendation.js
+++ Module5/travelRecommendation/travel_recommendation.js
@@ -1,25 +1,52 @@
+let content = document.getElementById("content");
+
 function home() {
-    let contenu = `
+    let home_content = `
+         <div class="home-container">
                   <h1>Welcome to Travel PAT</h1>
                   <p>TravelBloom is your ultimate travel guide, offering you the best destinations and experiences. Whether you're planning a trip or simply looking for inspiration, we've got you covered.</p>
                   <button class="book-now-button">BOOK NOW</button>
+                  </div>
     `;
-    document.getElementById("content").innerHTML = contenu;
+    content.innerHTML = home_content;
   }
 
 
   function aboutUs() {
-    let contenu = `
+    let aboutUs_content  = `
+                  <div class="aboutUs-container">
                   <h1>ABOUT US</h1>
-                  
+                  <p>We are a team of travel enthusiasts who believe that every destination is unique and deserves to be explored. Our mission is to provide you with the best travel experiences, whether you're planning a trip or simply looking for inspiration.</p>
+                  <div class="contacts-container" id="contacts-container">
+                   <h1>OUR TEAM</h1>
+                   ${contact("John Doe", "I'm a travel enthusiast who loves exploring new places.", "Software Engineer")}
+                   ${contact("Jane Smith", "I'm a software engineer who loves building innovative solutions.", "Project Manager")}
+                   ${contact("Mike Johnson", "I'm a travel enthusiast who loves exploring new places.", "Marketing Specialist")}
+                  </div>
+                </div>
     `;
-    document.getElementById("content").innerHTML = contenu;
+    content.innerHTML = aboutUs_content;
+  }
+
+  function contact(name, description, jobTitle ) {
+
+    let contact_content = `
+    <div class="contact-content">
+    <p><b>${name}</b></p>
+    <p>${description}</p>
+    <p><button class="job-title-button">${jobTitle}</button></p>
+    </div>
+    `;
+
+    return contact_content;
   }
 
   function contactUs() {
-    let contenu = `
-                  <h1>Contact Us</h1>     `;    
-    document.getElementById("content").innerHTML = contenu;
+    let contactUs_content = `
+                  <h1>Contact Us</h1>     `;
+
+
+    content.innerHTML = contactUs_content;
   }
 
 
